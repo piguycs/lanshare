93d14c2f2e3964561279d22031603ac8e48dba35
piguycs <piguycs@pm.me>
2024-12-29 19:34:52 +0100

    s2n-quic instead of quiche easier to get started with, and more ecosystem compat

--- crates/relay-server/src/main.rs
+++ crates/relay-server/src/main.rs
@@ -1,6 +1,26 @@
 //! Relay-Server for LANShare
 //! W.I.P. server for connecting LANShare clients
 
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate tracing;
+
+use s2n_quic::Server;
+
+type BoxError = Box<dyn std::error::Error>;
+
+const SOCKET_ADDR: &str = "0.0.0.0:4433";
+
+#[tokio::main]
+async fn main() -> Result<(), BoxError> {
+    tracing_subscriber::fmt::init();
+
+    let mut server = Server::builder().with_io(SOCKET_ADDR)?.start()?;
+
+    trace!("accepting connections");
+    while let Some(mut connection) = server.accept().await {
+        info!("hello world");
+        //
+    }
+
+    Ok(())
 }
--- justfile
+++ justfile
@@ -3,3 +3,6 @@
 
 @run-daemon:
     cargo build -p ls-daemon && pkexec target/debug/ls-daemon
+
+@run-server:
+    cargo run -p relay-server
